27205af8aa20ddbabb1f2ef91ac9da27354ef029
Viktoria <viktoria.ulianova@mail.ru>
2021-05-23 19:00:34 +0700

    Fixed

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,64 +1,63 @@
 function calcSum() {
     let num1, num2, result;
-    num1 = document.getElementById('n1').value, 
-    num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value,
+        num1 = parseInt(num1);
 
-    
-    num2 = document.getElementById('n2').value, 
-    num2 = parseInt(num2)
-    
+
+    num2 = document.getElementById('n2').value,
+        num2 = parseInt(num2)
+
     result = num1 + num2;
-    
+
     document.getElementById('result').innerHTML = result;
 }
 
 function calcMinus() {
     let num1, num2, result;
-    num1 = document.getElementById('n1').value, 
-    num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value,
+        num1 = parseInt(num1);
 
-    
-    num2 = document.getElementById('n2').value, 
-    num2 = parseInt(num2)
-    
+
+    num2 = document.getElementById('n2').value,
+        num2 = parseInt(num2)
+
     result = num1 - num2;
-    
+
     document.getElementById('result').innerHTML = result;
 }
 
 function calcMultiply() {
     let num1, num2, result;
-    num1 = document.getElementById('n1').value, 
-    num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value,
+        num1 = parseInt(num1);
 
-    
-    num2 = document.getElementById('n2').value, 
-    num2 = parseInt(num2)
-    
+
+    num2 = document.getElementById('n2').value,
+        num2 = parseInt(num2)
+
     result = num1 * num2;
-    
+
     document.getElementById('result').innerHTML = result;
 }
 
 function calcSplit() {
     let num1, num2, result;
-    num1 = document.getElementById('n1').value, 
-    num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value,
+        num1 = parseInt(num1);
 
-    
-    num2 = document.getElementById('n2').value, 
-    num2 = parseInt(num2)
-    
+
+    num2 = document.getElementById('n2').value,
+        num2 = parseInt(num2)
+
     result = num1 / num2;
-    
+
     document.getElementById('result').innerHTML = result;
 
     if (num2 == 0) {
 
-    alert("На ноль делить нельзя"); 
-    }
-
-    else {
-    (result);
+        alert("На ноль делить нельзя");
+    } else {
+        result = num1 / num2;
+        document.getElementById('out').innerHTML = result;
     }
 }
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,20 +1,20 @@
 function registration() {
-    let name, phone, age, country, email, password;
 
-    name = document.getElementById('username').value;
-    phone = document.getElementById('phonenumber').value;
-    age = document.getElementById('age').value;
-    country = document.getElementById('country').value;
-    email = document.getElementById('mail').value;
-    password = document.getElementById('userpass').value;
+    let name = document.getElementById('username').value;
+    let phone = document.getElementById('phonenumber').value;
+    let age = document.getElementById('age').value;
+    let country = document.getElementById('country').value;
+    let email = document.getElementById('mail').value;
+    let password = document.getElementById('userpass').value;
 
     phone = parseInt(phone);
-    // age = parseInt(age);
-    console.log(age)
+
     if (name == '') {
         alert("Empty name. Please tell your name");
     } else if (phone.length < 10) {
         alert("Uncorrect phone. Please tell your phone number");
+    } else if (age == '') {
+        alert("Empty place. How old are you?");
     } else if (age < 18) {
         alert("You are too young. Come back later");
     } else if (email == '') {
