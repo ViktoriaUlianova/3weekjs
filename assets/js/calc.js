cb47ec17df707f13fd8eda3ab5dce3c105a0fd69
Viktoria <viktoria.ulianova@mail.ru>
2021-05-23 23:19:49 +0700

    fixed calc

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,11 +1,11 @@
 function calcSum() {
     let num1, num2, result;
-    num1 = document.getElementById('n1').value,
-        num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
 
 
-    num2 = document.getElementById('n2').value,
-        num2 = parseInt(num2)
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2)
 
     result = num1 + num2;
 
@@ -14,12 +14,12 @@ function calcSum() {
 
 function calcMinus() {
     let num1, num2, result;
-    num1 = document.getElementById('n1').value,
-        num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
 
 
-    num2 = document.getElementById('n2').value,
-        num2 = parseInt(num2)
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2)
 
     result = num1 - num2;
 
@@ -28,36 +28,32 @@ function calcMinus() {
 
 function calcMultiply() {
     let num1, num2, result;
-    num1 = document.getElementById('n1').value,
-        num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
 
 
-    num2 = document.getElementById('n2').value,
-        num2 = parseInt(num2)
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2)
 
     result = num1 * num2;
 
     document.getElementById('result').innerHTML = result;
 }
 
-function calcSplit() {
+function calcDivide() {
     let num1, num2, result;
-    num1 = document.getElementById('n1').value,
-        num1 = parseInt(num1);
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
 
 
-    num2 = document.getElementById('n2').value,
-        num2 = parseInt(num2)
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2)
+    if (num2 == 0) {
+        alert("На ноль делить нельзя");
+        return;
+    }
 
     result = num1 / num2;
 
     document.getElementById('result').innerHTML = result;
-
-    if (num2 == 0) {
-
-        alert("На ноль делить нельзя");
-    } else {
-        result = num1 / num2;
-        document.getElementById('out').innerHTML = result;
-    }
 }
